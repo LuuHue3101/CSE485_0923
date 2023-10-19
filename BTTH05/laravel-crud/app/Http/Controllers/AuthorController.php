e119f09cd9ba21778c5013c3ab5c04b721876908
Lưu Xuân Huệ <luuhue257@gmail.com>
2023-10-19 19:47:11 +0700

    Create AuthorController.php

---
+++ BTTH05/laravel-crud/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $authors = Author::all();
+        return view('authors.index', compact('authors'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('authors.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = $request->validate([
+            'name' => 'required',
+            'bio' => 'required',
+        ]);
+        // Author::create($request->all());
+        $author = new Author();
+        $author->name = $validator['name'];
+        $author->bio = $validator['bio'];
+        $author->save();
+        return redirect()->route('authors.index')->with('success', 'Author Created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Author $author)
+    {
+        return view('authors.show', compact('author'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Author $author)
+    {
+        return view('authors.edit', compact('author'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Author $author)
+    {
+        $validator = $request->validate([
+            'name' => 'required',
+            'bio' => 'required',
+        ]);
+        // $author =  Author::find($id);
+        $author->name = $validator['name'];
+        $author->bio = $validator['bio'];
+        $author->save();
+        return redirect()->route('authors.index')->with('success', 'Author Created successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Author $author)
+    {
+        $author->delete();
+        return redirect()->route('authors.index')->with('success', 'Author delete successfully');
+    }
+}
